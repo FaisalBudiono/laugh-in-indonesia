9b7130d9ba6ea55253237e5aad4c27adc00bce79
FaisalBudiono <ucul.project@gmail.com>
2020-09-26 03:10:23 +0700

    Add hehe command
    The function to handle the commands is separated into multiple files to
    ensure adding another command will be easier in the future.

---
+++ app.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import Discord from 'discord.js';
+import command from './src/command';
+
+dotenv.config();
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('message', async (message) => {
+  console.log('message received by server');
+  await command(message);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
